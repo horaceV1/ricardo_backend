b956498ec9aafd55fb29f1a8a53e99f384f6b753
horaceV1 <danielfilipep18@gmail.com>
2026-02-10 14:29:10 -0100

    Fix submission list to properly display file data with download links

--- web/modules/custom/formulario_candidatura_dinamico/src/DynamicFormSubmissionListBuilder.php
+++ web/modules/custom/formulario_candidatura_dinamico/src/DynamicFormSubmissionListBuilder.php
@@ -33,7 +33,9 @@ class DynamicFormSubmissionListBuilder extends EntityListBuilder {
    */
   public function buildHeader() {
     $header['id'] = $this->t('ID');
+    $header['form_id'] = $this->t('Form');
     $header['email'] = $this->t('Email');
+    $header['data'] = $this->t('Data');
     $header['approval_status'] = $this->t('Status');
     $header['created'] = $this->t('Data de submissão');
     return $header + parent::buildHeader();
@@ -45,7 +47,38 @@ class DynamicFormSubmissionListBuilder extends EntityListBuilder {
   public function buildRow(EntityInterface $entity) {
     /** @var \Drupal\formulario_candidatura_dinamico\Entity\DynamicFormSubmission $entity */
     $row['id'] = $entity->id();
+    $row['form_id'] = $entity->getFormId();
     $row['email'] = $entity->getEmail();
+    
+    // Format the data column to show file information
+    $data = $entity->getData();
+    $data_output = [];
+    
+    if (!empty($data) && is_array($data)) {
+      foreach ($data as $field_name => $field_data) {
+        if (is_array($field_data) && isset($field_data['type']) && $field_data['type'] === 'file') {
+          // File field
+          $file_id = $field_data['value'] ?? null;
+          if ($file_id && is_numeric($file_id)) {
+            $file = \Drupal::entityTypeManager()->getStorage('file')->load($file_id);
+            if ($file) {
+              $filename = $field_data['filename'] ?? $file->getFilename();
+              $url = $file->createFileUrl();
+              $data_output[] = '<strong>' . $field_name . ':</strong> <a href="' . $url . '" target="_blank">' . $filename . '</a>';
+            }
+          }
+        } elseif (!is_array($field_data)) {
+          // Simple text field
+          $data_output[] = '<strong>' . $field_name . ':</strong> ' . htmlspecialchars($field_data);
+        }
+      }
+    }
+    
+    $row['data'] = [
+      'data' => [
+        '#markup' => !empty($data_output) ? implode('<br>', $data_output) : $this->t('No data'),
+      ],
+    ];
     
     // Add status badge
     $status = $entity->getApprovalStatus();
